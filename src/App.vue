4b8db1f6a490ca337fd53b85da997201b5423831
T.H <Thomashendrikz@Gmail.com>
2020-08-19 16:59:46 +1200

    home page grid bars are in place and links working

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,18 @@ export default {
 </script>>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: "Open Sans", sans-serif;
+}
+a {
+  text-decoration: none;
+}
+li {
+  list-style-type: none;
 }
 </style>
