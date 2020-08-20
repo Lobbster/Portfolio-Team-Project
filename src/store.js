edd62b9ded35145c34cf84ba319b23ac158616c6
T.H <Thomashendrikz@Gmail.com>
2020-08-20 20:40:02 +1200

    portfolio  added porject layout and skill

--- src/store.js
+++ src/store.js
@@ -4,34 +4,37 @@ export default {
       name: "Thom",
       id: 1,
       slug: "tom",
+      skills: ["graphic", "ux/ui", "web"],
       projects: [
         { name: "The Cake Project", slug: "TheCakeProject", detail: "12345" },
         { name: "Travle the world", slug: "Awsome", detail: "12345" },
         { name: "Nice!", slug: "Nice!", detail: "12345" },
-        { name: "dead", slug: "Dead", detail: "12345" }
-      ]
+        { name: "dead", slug: "Dead", detail: "12345" },
+      ],
     },
     {
       name: "Sylvia",
       id: 2,
       slug: "sylvia",
+      skills: ["graphic", "ux/ui", "web"],
       projects: [
         { name: "project-1", slug: "project-1", detail: "12345" },
         { name: "project-2", slug: "project-2", detail: "12345" },
         { name: "project-3", slug: "project-3", detail: "12345" },
-        { name: "project-4", slug: "project-4", detail: "12345" }
-      ]
+        { name: "project-4", slug: "project-4", detail: "12345" },
+      ],
     },
     {
       name: "Vinny",
       id: 3,
       slug: "vinny",
+      skills: ["graphic", "ux/ui", "web"],
       projects: [
         { name: "project-1", slug: "project-1", detail: "12345" },
         { name: "project-2", slug: "project-2", detail: "12345" },
         { name: "project-3", slug: "project-3", detail: "12345" },
-        { name: "project-4", slug: "project-4", detail: "12345" }
-      ]
-    }
-  ]
+        { name: "project-4", slug: "project-4", detail: "12345" },
+      ],
+    },
+  ],
 };
--- src/views/PortfolioTemplate.vue
+++ src/views/PortfolioTemplate.vue
@@ -1,23 +1,49 @@
 <template>
   <div>
-    <section class="portfolio">
-      <h1>{{ portfolio.name }}</h1>
+    <section>
+      <div class="skil-container">
+        <h1 class="skill-title">{{portfolio.skills[0]}}</h1>
+        <h1 class="skill-title">{{portfolio.skills[1]}}</h1>
+        <h1 class="skill-title">{{portfolio.skills[2]}}</h1>
+      </div>
     </section>
     <section>
-      <div class="cards">
-        <div
-          v-for="project in portfolio.projects"
-          :key="project.slug"
-          class="card"
-        >
+      <div class="portfolio">
+        <div v-for="project in portfolio.projects" :key="project.slug" class="card-container">
+          <h1 class="project-title">{{project.name}}</h1>
           <router-link
             :to="{
               name: `ProjectTemplate`,
               params: { projectSlug: project.slug }
             }"
-          >
-            <p>{{ project.name }}</p>
-          </router-link>
+          ></router-link>
+          <div class="cards">
+            <router-link
+              :to="{
+              name: `ProjectTemplate`,
+              params: { projectSlug: project.slug }
+            }"
+            >
+              <div class="image-card"></div>
+            </router-link>
+
+            <router-link
+              :to="{
+              name: `ProjectTemplate`,
+              params: { projectSlug: project.slug }
+            }"
+            >
+              <div class="image-card"></div>
+            </router-link>
+            <router-link
+              :to="{
+              name: `ProjectTemplate`,
+              params: { projectSlug: project.slug }
+            }"
+            >
+              <div class="image-card"></div>
+            </router-link>
+          </div>
         </div>
       </div>
     </section>
@@ -45,11 +71,40 @@ export default {
 </script>
 
 <style scoped>
-.card {
-  height: 100px;
-  width: 100px;
-  margin: 10px;
-  background-color: aquamarine;
-  cursor: pointer;
+.skil-container {
+  width: 675px;
+  height: 275px;
+  margin: 200px 80px;
+  background-color: grey;
+}
+
+.portfolio {
+  margin-top: 200px;
+  width: 100%;
+}
+.card-container {
+  margin-bottom: 100px;
+  position: relative;
+}
+.cards {
+  display: flex;
+  overflow: hidden;
+}
+.image-card {
+  width: 600px;
+  height: 400px;
+  margin: 5px;
+  background-color: teal;
+}
+.project-title {
+  font-size: 150px;
+  color: white;
+  font-family: "Sail", cursive;
+  text-transform: lowercase;
+  z-index: 1;
+  position: absolute;
+
+  bottom: -40px;
+  right: 0px;
 }
 </style>
